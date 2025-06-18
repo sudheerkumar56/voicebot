e6d3795de591e9ee8623366974acc9f60b3f6bf6
aumraj005 <128635875+aumraj005@users.noreply.github.com>
2025-06-18 10:37:55 +0530

    vercel added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { CohereClient } = require('cohere-ai');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
